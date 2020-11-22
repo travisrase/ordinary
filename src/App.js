cfecbb5acf159421ed1427e5548cc934c1381867
traivsrase <twrase@bowdoin.edu>
2020-11-22 16:54:51 -0700

    ugh

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ import ordlips_image from './images/ordlips.jpg';
 import mona_image from './images/mona.png';
 import ordinary_image from './images/ordinary.png';
 import secret_image from './images/secret.png';
-import fish_gif from './images/fishies.gif';
+import gallery_image from './images/gallery.jpg';
 import error_popup_gif from './images/error_popup.gif';
 import './App.css';
 
@@ -16,13 +16,20 @@ function App() {
 
   return (
     <div className="App">
-        <Box>
+        <Box display="flex" justifyContent="center">
           <div style={{ width: '100%' }}>
             <Box display="flex" justifyContent="center" m={1} p={1}>
-              <Box p={1} width={3/4}>
-                <text className="willieText">willie</text>
-                <img src={code_image} className="code-image"/>
-                <img src={error_popup_gif} className='fish'/>
+              <Box p={1}>
+                <Box display='flex'>  
+                  <Box justifyContent="flex-end" height={1}>
+                    <text className="willieText">willie</text>
+                  </Box>
+                  <img src={code_image} className="code-image"/>
+                  <img src={error_popup_gif} className='fish'/>
+                </Box>
+                <Box>
+                  <img src={gallery_image} className='gallery-image'/>
+                </Box>
                 <text className="infoText">sketches come in hoodies</text>
                 <br/>
                 <text className="infoText">to order text 720.251.6343</text>
